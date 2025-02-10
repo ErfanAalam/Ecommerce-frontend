7e327f6456cf16eb2ae121e5c38446f3636b3a4b
ErfanAalam <erfankhansiwani@gmail.com>
2025-02-10 23:08:44 +0530

    mui added to vite config file

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ["@mui/material"],
+  },
   resolve: {
     alias: {
       'react-router': 'react-router-dom', // Force correct package usage
+      "@mui/material": "@mui/material/esm",
     },
   },
 })
