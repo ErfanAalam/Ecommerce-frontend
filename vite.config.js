ac7f1a49fd4b61973c87cb4e29de3f54f56dbca5
ErfanAalam <erfankhansiwani@gmail.com>
2025-02-10 23:45:13 +0530

    vite congif js file recreated

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ["@mui/material"],
-  },
+  // optimizeDeps: {
+  //   include: ["@mui/material"],
+  // },
   resolve: {
     alias: {
-      'react-router': 'react-router-dom', // Force correct package usage
-      "@mui/material": "@mui/material/esm",
+      // 'react-router': 'react-router-dom', // Force correct package usage
+      // "@mui/material": "@mui/material/esm",
     },
   },
 })
