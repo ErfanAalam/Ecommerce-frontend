2ca12f15622b81eaff6dca13914a71ceac15cf74
ErfanAalam <erfankhansiwani@gmail.com>
2025-02-10 22:35:48 +0530

    react-router-dom downgraded

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import commonjs from '@rollup/plugin-commonjs'
-import nodeResolve from '@rollup/plugin-node-resolve'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodeResolve(),commonjs()],
-  optimizeDeps: {
-    include: ["react-router-dom"], // Force Vite to pre-bundle
-  },
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-  },
+  plugins: [react()],
 })
