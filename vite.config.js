5de8ce7b7fdde64303a8aae39f116c849c4b61a2
ErfanAalam <erfankhansiwani@gmail.com>
2025-02-11 09:40:39 +0530

    mui upgraded and vite config updated

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // optimizeDeps: {
-  //   include: ["@mui/material"],
-  // },
+  optimizeDeps: {
+    // exclude: ["@mui/material"],
+  },
   resolve: {
     alias: {
       // 'react-router': 'react-router-dom', // Force correct package usage
