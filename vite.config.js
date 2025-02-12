0df8c3a5280eb44ca9bbf449ead65b121b158cab
ErfanAalam <erfankhansiwani@gmail.com>
2025-02-12 09:20:56 +0530

    vite config updated and node_module reinstalled

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : "/",
-  optimizeDeps: {
-    // exclude: ["@mui/material"],
-  },
-  resolve: {
-    alias: {
-      // 'react-router': 'react-router-dom', // Force correct package usage
-      // "@mui/material": "@mui/material/esm",
-    },
-  },
 })
